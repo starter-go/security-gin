bae4648ab0e468cae9ec188ad06d9c3349745f5c
MEZ <mez@bitwormhole.com>
2023-10-31 11:47:03 +0800

    up to v0.0.8

--- module_t.go
+++ module_t.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	theModuleName        = "github.com/starter-go/security-gin"
-	theModuleVersion     = "v0.0.7"
-	theModuleRevision    = 7
+	theModuleVersion     = "v0.0.8"
+	theModuleRevision    = 8
 	theModuleResPath     = "src/main/resources"
 	theTestModuleResPath = "src/test/resources"
 )
